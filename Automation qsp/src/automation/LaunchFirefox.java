8fbc48d12c9e6e140d2d9238128522532487da4e
varta <varta@LAPTOP-UV1SJ1L8>
2021-09-17 21:43:22 +0530

    hello this is automation

---
+++ Automation qsp/src/automation/LaunchFirefox.java
@@ -0,0 +1,12 @@
+package automation;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchFirefox {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+		new FirefoxDriver();
+		
+	}
+
+}
